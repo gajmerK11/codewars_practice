af4a7a4562828848609d62fa99b6980f2a1c1c65
gajmerkumar <gajmerk17@gmail.com>
2025-07-08 17:20:27 +0545

    Tue, Jul-8

---
+++ merge_strings_alternately.py
@@ -0,0 +1,45 @@
+"""
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+
+"""
+
+def merge_alternately(word1, word2):
+    merged = ""
+    min_len = min(len(word1), len(word2))
+
+    for char in range(min_len):
+        merged += word1[char] + word2[char]
+
+    if len(word1) > len(word2):
+        merged += word1[min_len:]
+    elif len(word2) > len(word1):
+        merged += word2[min_len:]
+
+    return merged
